7c1cbb79fe188b396de0eaf4237d17eeafdbdf96
fatura-brigitta <fatura.brigitta@students.jedlik.eu>
2026-04-23 11:23:55 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -102,7 +102,7 @@ export default function Home() {
         <div className="grid grid-cols-2 gap-6">
 
           <div>
-            <h2 className="font-semibold text-blue-600 mb-3 text-center">
+            <h2 className="font-semibold text-blue-200 mb-3 text-center">
               👦 Fiúk
             </h2>
               <div className="space-y-1">
