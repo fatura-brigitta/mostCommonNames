5fd619d526b71837f45e97ea4b73d80eb835e213
fatura-brigitta <fatura.brigitta@students.jedlik.eu>
2026-04-24 08:15:54 +0200

    last fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Password verify",
-  description: "Password verifying",
+  title: "MostCommonNames",
+  description: "MostCommonNames",
 };
 
 export default function RootLayout({
